bee52317982472368c91013e2e610620f9d6c810
Diego Pardo <diegopar03@gmail.com>
2023-02-07 10:22:43 -0600

    Sending Emails

--- helpers/email.js
+++ helpers/email.js
@@ -1,54 +1,72 @@
 import nodemailer from 'nodemailer'
 
-const emailRegistro = async (datos) => {
+const emailRegistro = async datos => {
+    const { nombre, email, token } = datos;
 
-    const { email, nombre, token } = datos;
+    const client = Sib.ApiClient.instance
+    const apiKey = client.authentications['api-key']
+    apiKey.apiKey = process.env.API_KEY
 
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
+    const tranEmailApi = new Sib.TransactionalEmailsApi()
+    const sender = {
+        email: 'diegopar03@gmail.com',
+        name: 'Diego Pardo',
+    }
+    const receivers = [
+        {
+            email,
+        },
+    ]
 
-    const info = await transporter.sendMail({
-        from: 'UpTask - Administrador de Proyectos <cuentas@uptask.com>',
-        to: email,
-        subject: 'UpTask - Confirma tu Cuenta',
-        text: 'Confirma tu cuenta en UpTask',
-        html: `<p>Hola: ${nombre}, confirma tu cuenta en UpTask</p>
-               <p>Tu cuenta esta casi lista, solo debes comprobarla en el siguiente enlace:</p>
-               <a href='http://127.0.0.1:5173/confirmar/${token}'>Confirmar Cuenta</a>
-               <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje.</p>
+    tranEmailApi
+    .sendTransacEmail({
+        sender,
+        to: receivers,
+        subject: 'UpTask - Confirma tu cuenta',
+        htmlContent: `
+            <p style="display:block; color:#374151; font-weight:bold; text-transform:uppercase;">Hola: ${nombre}, confirma tu cuenta en UpTask</p>
+            <p style="display:block; color:#374151; font-weight:bold; text-transform:uppercase;">Tu cuenta esta casi lista, solo debes comprobarla en el siguiente enlace:</p>
+            <a style="background-color:#4f46e5; font-weight:bold; color:white; padding:10px; border-radius:5px; text-decoration:none; text-transform:uppercase; margin-top:20px; margin-bottom:20px;" href="http://${process.env.FRONTEND_URL}/confirmar/${token}">Confirmar Cuenta</a>
+            <p style="display:block; color:#374151; font-weight:bold; text-transform:uppercase;">Si tu no creaste esta cuenta, puedes ignorar este mensaje<p>
         `
-    });
+    })
+    .then(console.log)
+    .catch(console.log)
 }
 
 const emailRecuperarPassword = async (datos) => {
-    const { email, nombre, token } = datos;
 
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
+    const { nombre, email, token } = datos;
 
-    const info = await transporter.sendMail({
-        from: 'UpTask - Administrador de Proyectos <cuentas@uptask.com>',
-        to: email,
-        subject: 'UpTask - Reestablece tu Password',
-        text: 'Reestablece tu password en UpTask',
-        html: `<p>Hola: ${nombre}, has solicitado reestablecer tu password</p>
-               <p>Sigue el siguiente enlace para generar un nuevo password:</p>
-               <a href='http://127.0.0.1:5173/olvide-password/${token}'>Reestablecer Password</a>
-               <p>Si tu no solicitaste este email, puedes ignorar este mensaje.</p>
+    const client = Sib.ApiClient.instance
+    const apiKey = client.authentications['api-key']
+    apiKey.apiKey = process.env.API_KEY
+
+    const tranEmailApi = new Sib.TransactionalEmailsApi()
+    const sender = {
+        email: 'diegopar03@gmail.com',
+        name: 'Diego Pardo',
+    }
+    const receivers = [
+        {
+            email,
+        },
+    ]
+
+    tranEmailApi
+    .sendTransacEmail({
+        sender,
+        to: receivers,
+        subject: 'UpTask - Reestablece tu contraseña',
+        htmlContent: `
+            <p style="display:block; color:#374151; font-weight:bold; text-transform:uppercase;">Hola: ${nombre}, has solicitado reestablecer tu contraseña</p>
+            <p style="display:block; color:#374151; font-weight:bold; text-transform:uppercase;">Sigue el siguiente enlace para reestablecer tu contraseña:</p>
+            <a style="background-color:#4f46e5; font-weight:bold; color:white; padding:10px; border-radius:5px; text-decoration:none; text-transform:uppercase; margin-top:20px; margin-bottom:20px;" href="http://${process.env.FRONTEND_URL}/olvide-password/${token}">Confirmar Cuenta</a>
+            <p style="display:block; color:#374151; font-weight:bold; text-transform:uppercase;">Si tu no solicitaste este email, puedes ignorar este mensaje<p>
         `
-    });
+    })
+    .then(console.log)
+    .catch(console.log)
 }
 
 export{
